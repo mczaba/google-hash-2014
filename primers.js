8006af60fc53ceb6d0e2583dab289822887f3b73
Martin C <martin.czabadaj@gmail.com>
2021-03-31 16:05:33 +0200

    2nd try : 6861 (super slow alho)

--- primers.js
+++ primers.js
@@ -7,47 +7,59 @@ const maparray = textByLine.map(line => line.split(""));
 
 const result = [];
 
-function checkSquare(i,j,size) {
-    let result = true
-    if (i + size > length || j + size > width) return false
-    for (let k = 0; k < size; k++ ) {
-        for (let l = 0; l < size; l++) {
-            if (maparray[i+k][j+l] !== '#') result = false
-        }
+function checkSquare(i, j, size) {
+  let result = true;
+  if (i + size > length || j + size > width) return false;
+  for (let k = 0; k < size; k++) {
+    for (let l = 0; l < size; l++) {
+      if (maparray[i + k][j + l] !== "#") result = false;
     }
-    return result
+  }
+  return result;
 }
 
-function clearSquare(i,j,size) {
-    for (let k = 0; k < size; k++ ) {
-        for (let l = 0; l < size; l++) {
-            maparray[i+k][j+l] = '*'
-        }
-    }
-}
-
-function treatPixel(i,j) {
-    let size = 1
-    while (checkSquare(i,j,size+1)) {
-        if (i + size + 1 >= length || j + size + 1 >= width) break
-        size++
-    }
-    clearSquare(i,j,size)
-    return `FILL,${j},${i},${size}`
-}
-
-let fillResult = () => {
-  for (let i = 0; i < length; i++) {
-    for (let j = 0; j < width; j++) {
-        if (maparray[i][j] === '#') result.push(treatPixel(i,j))
+function clearSquare(i, j, size) {
+  for (let k = 0; k < size; k++) {
+    for (let l = 0; l < size; l++) {
+      maparray[i + k][j + l] = "*";
     }
   }
 }
 
-fillResult()
+function getMaxSizeByIndex(i, j) {
+    if (maparray[i][j] !== "#") return 0;
+    let size = 1;
+    while (checkSquare(i, j, size + 1)) {
+        size++;
+    }
+    return size;
+}
 
-fs.writeFileSync('output.txt', result.join('\n'))
+function getMaxSquare() {
+    let sizeMap = new Map();
+    for (let i = 0; i < length; i++) {
+        for (let j = 0; j < width; j++) {
+            sizeMap.set([i, j], getMaxSizeByIndex(i, j));
+        }
+    }
+    const [index, size] = [...sizeMap.entries()].reduce((a, e) => e[1] > a[1] ? e : a);
+    return { index, size };
+}
+
+const fillResult = () => {
+    while(getMaxSquare().size > 0) {
+        const i = getMaxSquare().index[0]
+        const j = getMaxSquare().index[1]
+        const size = getMaxSquare().size
+        console.log(`FILL,${j},${i},${size}`)
+        result.push(`FILL,${j},${i},${size}`)
+        clearSquare(i,j,size)
+    }
+}
+
+fillResult()
 
 console.log(result.length)
 console.log(result)
 
+fs.writeFileSync("output.txt", result.join("\n"));
